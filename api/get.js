ddbee0825156e7b843735f1008dfa31f460ef4db
Anyrooooo <10919987@my.uvu.edu>
2024-02-21 23:22:46 -0700

    changing the get.js file again

--- api/get.js
+++ api/get.js
@@ -1,7 +1,10 @@
-import { readFile } from 'fs/promises';
-import { join } from 'path';
+import { readFile } from 'fs/promises'
+import { join, dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-const filePath = join(__dirname, '..', 'src', 'data', 'FullEventList.json');
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+const filePath = join(__dirname, '..', 'src', 'data', 'FullEventList.json')
 
 export default async (req, res) => {
     const data = await readFile(filePath, 'utf8')
