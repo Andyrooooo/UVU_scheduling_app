2ee67488858b4e9e378c2f425363e27f4e3edacf
Anyrooooo <10919987@my.uvu.edu>
2024-02-21 23:42:55 -0700

    not working so I am using example to make sure there is no syntax errors

--- api/delete.js
+++ api/delete.js
@@ -1,13 +1,13 @@
-import { writeFile } from 'fs/promises'
-import { join, dirname } from 'path'
-import { fileURLToPath } from 'url'
+import { writeFile } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-const filePath = join(__dirname, '..', 'src', 'data', 'FullEventList.json')
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = join(__dirname, '..', 'src', 'data', 'FullEventList.json');
 
 export default async (req, res) => {
     await writeFile(filePath, JSON.stringify([]));
-    res.send({"message": "data Removed part 2"})
-    console.log("data Removed part 2")
+    res.send({"message": "data Removed part 2"});
+    console.log("data Removed part 2");
 }
