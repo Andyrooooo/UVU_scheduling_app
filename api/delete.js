d5a9590763afce0b64fb597e4df5f6e1d9740892
Anyrooooo <10919987@my.uvu.edu>
2024-02-21 23:33:35 -0700

    get.js is now working and applying same concepts to delete.js

--- api/delete.js
+++ api/delete.js
@@ -1,7 +1,13 @@
 import { writeFile } from 'fs/promises'
+import { join, dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+const filePath = join(__dirname, '..', 'src', 'data', 'FullEventList.json')
 
 export default async (req, res) => {
-    await writeFile('./src/data/FullEventList.json', JSON.stringify([]));
+    await writeFile(filePath, JSON.stringify([]));
     res.send({"message": "data Removed part 2"})
     console.log("data Removed part 2")
 }
